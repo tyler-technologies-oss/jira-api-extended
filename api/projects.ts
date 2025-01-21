d6264ce383c0a15600c00d700826a787842e94c8
Logan Brown <logan.brown@tylertech.com>
2025-01-21 15:45:15 -0500

    Rename get to list in projects api

--- api/projects.ts
+++ api/projects.ts
@@ -3,7 +3,7 @@ import HTTP from "./http";
 export default class Projects {
     constructor(private httpservice: HTTP, public config: IJiraAPI) {}
 
-    public get(projectKey: string): Promise<IObject> {
+    public list(projectKey: string): Promise<IObject> {
         const url = new URL(`${this.config.url}/rest/api/latest/project/${projectKey}`);
 
         return this.httpservice.get(url);

854269db2f1b60187df37a354678d27895588f4b
Logan Brown <logan.brown@tylertech.com>
2025-01-21 15:54:22 -0500

    Require number instead of string

--- api/projects.ts
+++ api/projects.ts
@@ -3,8 +3,8 @@ import HTTP from "./http";
 export default class Projects {
     constructor(private httpservice: HTTP, public config: IJiraAPI) {}
 
-    public list(projectKey: string): Promise<IObject> {
-        const url = new URL(`${this.config.url}/rest/api/latest/project/${projectKey}`);
+    public list(id: number): Promise<IObject> {
+        const url = new URL(`${this.config.url}/rest/api/latest/project/${id}`);
 
         return this.httpservice.get(url);
     }

63036c3d8537e15dea4053ad2d4a1c0631247f6d
Logan Brown <logan.brown@tylertech.com>
2025-12-09 11:14:43 -0500

    Update accounts.get

--- api/accounts.ts
+++ api/accounts.ts
@@ -4,15 +4,16 @@ export default class Accounts {
     constructor(private httpservice: HTTP, public config: IJiraAPI) {}
 
     public get(email: string): Promise<IObject> {
-        const url = new URL(`${this.config.url}/rest/api/3/users/search`);
+        const url = new URL(`${this.config.url}/rest/api/3/user/search`);
 
         const queryParams: IObject = {
             query: email,
             maxResults: "1"
         };
-        Object.entries(queryParams).forEach(([key, value]) => {
+
+        for (const [key, value] of Object.entries(queryParams)) {
             url.searchParams.append(key, value);
-        });
+        }
 
         return this.httpservice.get(url);
     }
